eb383fcd81f502e393a89745a011c52ce5d89339
derekjpeters <derek.j.peters@gmail.com>
2025-11-10 17:58:58 -0500

    updated routes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import gameRoutes from "./routes/gameRoutes.js"
+import teamRoutes from "./routes/teamRoutes.js"
 
 const app = express ();
 const PORT = 4000;
@@ -10,7 +11,8 @@ app.use(cors()); //allow our middleware to use CORS
 app.use(express.json()); //parse incoming JSON bodies
 
 //Routes
-app.use("/api/game", gameRoutes)
+app.use("/api/game", gameRoutes);
+app.use("/api/teams", teamRoutes);
 
 //Start Server
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
---
+++ server/routes/teamRoutes.js
@@ -0,0 +1,21 @@
+import express from 'express';
+const router = express.Router();
+
+const teams = [
+    {id: 1, code: "HOME", name: "Home Ravens", primaryColor: "#241773"},
+    {id: 2, code: "HOME", name: "Home Cowboys", primaryColor: "#002145"},
+    {id: 3, code: "AWAY", name: "Away DaBears", primaryColor: "#C83803"},
+    {id: 4, code: "AWAY", name: "Away Dolphins", primaryColor: "#008E98"}
+];
+
+router.get("/",(_req, res) => res.json(teams));
+
+router.get('/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const team = teams.find((t) => t.id === id)
+    if (!team) return res.status(404).json({error: "Team not found!"});
+    res.json(team);
+});
+
+export default router;
+
