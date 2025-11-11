bbeabc8bd9ea02564b58295c7a9562a7e21a3434
derekjpeters <derek.j.peters@gmail.com>
2025-11-10 21:33:37 -0500

    update routes

--- server/routes/gameRoutes.js
+++ server/routes/gameRoutes.js
@@ -3,17 +3,31 @@ const router = express.Router()
 
 //Mock data for get request
 let gameState = {
-    score: {HOME: 21, AWAY: 7},
-    possession: "AWAY",
-    quarter: 3,
-    status: "IN_PROGRESS"
+    score: {HOME: 0, AWAY: 0},
+    possession: "HOME",
+    quarter: 1,
+    status: "IN_PROGRESS",
+    yardline: 25, //0..10 left -> right
+    homeTeamName: "Home Ravens",
+    awayTeamName: "Away DaBears",
 };
 
-//GET -> Return Mock GameState
+//GET /api/game
 router.get("/", (req, res) => {
     console.log("Current gameState", gameState)
     res.json(gameState);
 });
+
+//Patch /api/game/score {team, delta}
+router.patch("/score", (req, res) => {
+    const { team, delta } = req.body || {};
+    if (team !== "HOME" & team !== "AWAY") 
+        return res.status(400).json({error: "Invalid team"});
+    gameState.score[team] += Number(delta) || 0; //{HOME: 0, AWAY: 0}
+    return res.json(gameState);
+});
+
+
 
 //POST -> Reset Game
 router.post("/reset", (req, res) => {
